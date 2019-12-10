7cba1cc2044eb29e39e8bccb0a128feebd127618
Edvard Thörnros <edvard.thornros@gmail.com>
2019-12-10 19:47:28 +0100

    DId day 2 in haskell... Curse you recursive definitions

--- haskell/day02.hs
+++ haskell/day02.hs
@@ -5,26 +5,26 @@ import Data.List.Split
 main :: IO()
 main = do
          content <- readFile "../input/day02"
-         let program = readInt . splitOn "," $ content
-         let program = replace 1 12 program
-         let program = replace 2 2 program
-         let program = evalProgram 0 program
-         print $ head program
+         let program = readInt $ splitOn "," content
+         let program_a = replace 1 12 program
+         let program_b = replace 2 2 program_a
+         let program_c = evalProgram 0 program_b
+         print $ head program_c
 
 readInt :: [String] -> [Int]
 readInt = map read
 
 replace :: Int -> Int -> [Int] -> [Int]
-replace 0 n (_:xs) = n : xs
-replace a n (q:xs) = q : replace (a - 1) n xs
+replace n a xs = take n xs ++ [a] ++ drop (n + 1) xs
 
 evalProgram :: Int -> [Int] -> [Int]
-evalProgram at prog | op == 1  = evalProgram (at + 4) $ evalAdd a b r prog
-                    | op == 2  = evalProgram (at + 4) $ evalMul a b r prog
-                    | op == 99 = prog
+evalProgram at prog | op == 1   = evalProgram (at + 4) $ evalAdd a b r prog
+                    | op == 2   = evalProgram (at + 4) $ evalMul a b r prog
+                    | op == 99  = prog
+                    | otherwise = undefined
                     where op = prog !! (at + 0)
-                          a  = prog !! (at + 1)
-                          b  = prog !! (at + 2)
+                          a  = prog !! (prog !! (at + 1))
+                          b  = prog !! (prog !! (at + 2))
                           r  = prog !! (at + 3)
 
 evalAdd :: Int -> Int -> Int -> [Int] -> [Int]
